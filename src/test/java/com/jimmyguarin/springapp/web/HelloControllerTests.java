bee4faad4ce41f6bba832713ed26eecce720b4fb
JimmyGuarin <jimmyguarin43@gmail.com>
2017-02-03 12:09:44 -0500

    1.0
    Aplicación Base y Configuración del Entorno

---
+++ src/test/java/com/jimmyguarin/springapp/web/HelloControllerTests.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jimmyguarin.springapp.web;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class HelloControllerTests {
+    
+    @Test
+    public void TesthandleRequestView() throws Exception{
+        
+        HelloController controller = new HelloController();
+        ModelAndView modelandView= controller.handleRequest(null,null);
+        assertEquals("hello.jsp",modelandView.getViewName());
+    }
+}
